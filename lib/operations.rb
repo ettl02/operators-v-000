bd22f99d4b11cb7c5b6fa3b9e9fcd773b53e4241
Emily Wessel <ettl02@gmail.com>
2018-06-18 15:36:04 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
 	speed = "#{speed}"
-	if "#{speed}" == <30 || "#{speed}" == >60
+	if "#{speed}" == < 30 || "#{speed}" == > 60
 			true
-		else 
-			false 
+		else
+			false
 end
 
 
